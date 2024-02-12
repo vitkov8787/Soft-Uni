67ab3cf85e8cd679c67cc559058f70fa2e3c31ce
vitkov8787 <59659361+vitkov8787@users.noreply.github.com>
2024-02-12 04:18:28 +0200

    List exercise 1 and 2

---
+++ Themes/Lists-Exercise/01.Train/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<int> list = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int max = int.Parse(Console.ReadLine());
+
+            
+
+            while(true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "end")
+                {
+                    break;
+                }
+                string[]comand = input.Split();
+                if (comand[0] == "Add")
+                {
+                    list.Add(int.Parse(comand[1]));
+                }
+                else
+                {
+                    int numPass = int.Parse(input);
+
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (list[i] + numPass <= max)
+                        {
+                            list[i] += numPass;
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(String.Join(" ",list));
+        }
+    }
+}
---
+++ Themes/Lists-Exercise/02.ChangeList/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _02.ChangeList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int>list = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                string[] command = input.Split();
+                if (input == "end")
+                {
+                    break;
+                }
+                switch (command[0])
+                {
+                    case "Delete":
+                        list.RemoveAll(item => item == int.Parse(command[1]));
+                        break;
+                    case "Insert":
+                        list.Insert(int.Parse(command[2]), int.Parse(command[1]));
+                        break;
+                }
+            }
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
