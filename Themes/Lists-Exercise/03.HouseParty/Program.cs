e66e3c0b04d7eba0e774a38e5fb12bb8768390ea
vitkov8787 <59659361+vitkov8787@users.noreply.github.com>
2024-02-12 12:34:08 +0200

    task3 exercise

---
+++ Themes/Lists-Exercise/03.HouseParty/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+
+namespace _03.HouseParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                string[] arg = input.Split();
+                //проверява дали поне един елемент в списъка list отговаря
+                //на условието item == arg[0].
+                bool has = list.Any(item => item == arg[0]);
+                if (arg[2] == "going!")
+                {
+                    if (!has)
+                    {
+                        list.Add(arg[0]);
+                    }
+                    else Console.WriteLine($"{arg[0]} is already in the list!");
+                }
+                else if (arg[2] == "not")
+                {
+                    if (has)
+                    {
+                        list.Remove(arg[0]);
+                    }
+                    if (!has)
+                    {
+                        Console.WriteLine($"{arg[0]} is not in the list!");
+                    }
+                }
+            }
+            Console.WriteLine(String.Join("\n", list));   
+        }
+    }
+}
+
+/*
+4
+Allie is going!
+George is going!
+John is not going!
+George is not going!
+*/
